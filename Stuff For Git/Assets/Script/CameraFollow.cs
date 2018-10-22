40e98c061961fe3b79c8e31d84a1958efa3e6e6e
poisonparodies <leahcholm@Knights.ucf.edu>
2018-10-21 23:58:47 -0400

    stuff
    for git

---
+++ Stuff For Git/Assets/Script/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform target;
+
+    public float cameraDistance = 30.0f;
+
+    void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+    // public Vector3 offset;
+    // public float smoothSpeed = 0.125f;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
---
+++ Stuff For Git/Assets/Script/CharacterAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour {
+
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("isWalking_Right", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking_Right", false);
+        }
+    }
+}
